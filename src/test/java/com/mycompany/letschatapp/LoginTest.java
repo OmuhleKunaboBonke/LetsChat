511b17ff97e2093fe4e2199040ecc3c208794ead
Student <Student@LabVM2049939.udhrizqs030efg0dqpoadcmnse.jnbx.internal.cloudapp.net>
2026-09-18 14:46:12 +0200

    My Lets Chat Application (1)

--- src/test/java/com/mycompany/letschatapp/LoginTest.java
+++ src/test/java/com/mycompany/letschatapp/LoginTest.java
@@ -78,6 +78,71 @@ public class LoginTest {
         assertTrue(result);
     }
 
- 
+    @Test
+    public void testLoginFailed() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean result =
+                user.loginUser("wrong_username", "wrong_password");
+
+        assertFalse(result);
+    }
+    
+    // REGISTRATION MESSAGE TEST
+    @Test
+    public void testPasswordSuccessMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        // Because all registration conditions are correct,
+        // registerUser() should return this message.
+        assertEquals(
+                "Registration successful.",
+                user.registerUser()
+        );
+    }
+
+    @Test
+    public void testPasswordFailureMessage() {
+
+        Login user = new Login("kyl_1", "password", "+27838968976", "Anele", "Mahlayeye");
+
+        assertEquals(
+                "Password is not correctly formatted; please ensure "
+                + "that the password contains at least eight characters, "
+                + "a capital letter, a number, and a special character.",
+                user.registerUser()
+        );
+    }
+    
+    // LOGIN MESSAGE TESTS
+     @Test
+    public void testSuccessfulLoginMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean loginResult =
+                user.loginUser("kyl_1", "Ch&&sec@ke99!");
+
+        assertEquals(
+                "Welcome Anele, Mahlayeye it is great to see you.",
+                user.returnLoginStatus(loginResult)
+        );
+    }
+
+    @Test
+    public void testFailedLoginMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean loginResult =
+                user.loginUser("wrong", "wrong");
+
+        assertEquals(
+                "Username or password incorrect, please try again.",
+                user.returnLoginStatus(loginResult)
+        );
+    }
 
 }
