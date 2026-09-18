e12bb01fdc4e9a12ef868765395b34be69fc2c56
Student <Student@LabVM2049939.udhrizqs030efg0dqpoadcmnse.jnbx.internal.cloudapp.net>
2026-09-18 10:14:53 +0200

    My Lets Chat Application (2)

--- src/main/java/com/mycompany/letschatapp/Login.java
+++ src/main/java/com/mycompany/letschatapp/Login.java
@@ -9,8 +9,46 @@ package com.mycompany.letschatapp;
  * @author Student
  */
 public class Login { 
+    private String registeredUsername;
+    private String registeredPassword;
     
+    private String firstName;
+    private String lastName;
     
+    // Constructor
+    public Login(String registeredUsername, String registeredPassword, String firstName, String lastName){
+        
+        this.registeredUsername = registeredUsername;
+        this.registeredPassword = registeredPassword;
+        this.firstName = firstName;
+        this.lastName = lastName;
 
+    }
+    
+    // Check the username and password entered during login
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+        
+        if (enteredUsername.equals(registeredUsername) && enteredPassword.equals(registeredPassword)){
+            
+            return true;
+        }else {
+            return false;
+        
+        }
+    }
+    
+    // Return the appropriate login message
+    public String returnLoginStatus(boolean loginSuccessful){
+        
+        if (loginSuccessful){
+            
+             return "Welcome " + firstName + ", " + lastName
+                    + " it is great to see you.";
 
+        } else {
+
+            return "Username or password incorrect, please try again.";
+
+        }
+    }
 }
