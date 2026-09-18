3f524b74645026374aa4cbb7c65e781f012fd56a
Student <Student@LabVM2049939.udhrizqs030efg0dqpoadcmnse.jnbx.internal.cloudapp.net>
2026-09-18 12:44:04 +0200

    My Lets Chat Application (6)

--- src/test/java/com/mycompany/letschatapp/LoginTest.java
+++ src/test/java/com/mycompany/letschatapp/LoginTest.java
@@ -13,17 +13,136 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class LoginTest {
     
+     @Test
+    public void testUsernameCorrectlyFormatted() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele","Mahlayeye");
+
+        assertTrue(user.checkUserName());
+    }
+    
     //Username Test
     @Test
     public void testUsernameCorrectly(){
         
-        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele",
-                "Mahlayeye");
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
         
         assertFalse(user.checkUserName());
 
     }
-
     
-   
+    //Password Test
+    @Test
+    public void testPasswordCorrectlyFormatted() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye"
+        );
+
+        assertTrue(user.checkPasswordComplexity());
+    }
+
+    @Test
+    public void testPasswordIncorrectlyFormatted() {
+
+        Login user = new Login("kyl_1", "password", "+27838968976", "Anele", "Mahlayeye");
+
+        assertFalse(user.checkPasswordComplexity());
+    }
+    
+     // CELLPHONE TESTS
+    @Test
+    public void testCellPhoneCorrectlyFormatted() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        assertTrue(user.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testCellPhoneIncorrectlyFormatted() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "08966553", "Anele", "Mahlayeye");
+
+        assertFalse(user.checkCellPhoneNumber());
+    }
+    
+     // LOGIN TESTS
+    @Test
+    public void testLoginSuccessful() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean result =
+                user.loginUser("kyl_1", "Ch&&sec@ke99!");
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testLoginFailed() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean result =
+                user.loginUser("wrong_username", "wrong_password");
+
+        assertFalse(result);
+    }
+    
+    // REGISTRATION MESSAGE TEST
+    @Test
+    public void testPasswordSuccessMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        // Because all registration conditions are correct,
+        // registerUser() should return this message.
+        assertEquals(
+                "Registration successful.",
+                user.registerUser()
+        );
+    }
+
+    @Test
+    public void testPasswordFailureMessage() {
+
+        Login user = new Login("kyl_1", "password", "+27838968976", "Anele", "Mahlayeye");
+
+        assertEquals(
+                "Password is not correctly formatted; please ensure "
+                + "that the password contains at least eight characters, "
+                + "a capital letter, a number, and a special character.",
+                user.registerUser()
+        );
+    }
+    
+    // LOGIN MESSAGE TESTS
+     @Test
+    public void testSuccessfulLoginMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean loginResult =
+                user.loginUser("kyl_1", "Ch&&sec@ke99!");
+
+        assertEquals(
+                "Welcome Anele, Mahlayeye it is great to see you.",
+                user.returnLoginStatus(loginResult)
+        );
+    }
+
+    @Test
+    public void testFailedLoginMessage() {
+
+        Login user = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Anele", "Mahlayeye");
+
+        boolean loginResult =
+                user.loginUser("wrong", "wrong");
+
+        assertEquals(
+                "Username or password incorrect, please try again.",
+                user.returnLoginStatus(loginResult)
+        );
+    }
+
 }
